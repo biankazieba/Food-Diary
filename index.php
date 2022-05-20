243a74a3c38edd88ec9ba3656fa18f50d3ffcf34
bianka.zieba <bianka.zieba@edu.tbz.ch>
2022-05-20 14:15:50 +0200

    created Homepage

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/style.css">
+    <link rel="shortcut icon" type="image/x-icon" href="/assets/img/logo.svg" />
+    <title>Homepage</title>
+</head>
+
+<body>
+
+    <!--Navbar-->
+
+    <div class="navbar">
+        <img class="logo" src="/assets/img/logo1.svg">
+        <button class="Home" href="">Home</button>
+        <button class="Diary" href="">Diary</button>
+        <button class="l" href="">Recipes</button>
+        <button class="login" href="">Login</button>
+    </div>
+
+    <!--Flexboxen-->
+    <div class="container">
+        <div class="box1">
+            <img class="foodimg" src="/assets/img/foodimg.jpg">
+        </div>
+        <div class="box2"></div>
+        <div class="box3">
+            <img class="logoimg" src="/assets/img/logo.svg">
+        </div>
+        <img class="pcimg" src="/assets/img/pc.png">
+        <div class="pc">
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous">
+    </script>
+    <script src="assets/script.js"></script>
+</body>
+
+</html>
